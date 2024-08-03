bd374573d6922267e9d99087ed39c901a3332cc7
jhenriquem <machadojh11@gmail.com>
2024-08-03 17:13:10 -0300

    final version

--- Program.cs
+++ Program.cs
@@ -1,64 +1,120 @@
-﻿class Tic_Tac_Toe
+﻿class TicTacToe
 {
-
   static void Main(string[] args)
   {
-
     char[,] table = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
-    char checkWinner()
+
+    char checkWinner(char[,] board)
     {
-      char returnValue = ' ';
       for (int i = 0; i < 3; i++)
       {
         // Column check
-        if (table[0, i] == table[1, i] & table[0, i] == table[2, i])
+        if (board[0, i] == board[1, i] && board[0, i] == board[2, i] && board[0, i] != ' ')
         {
-          returnValue = table[0, i];
+          return board[0, i];
         }
         // Line check
-        else if (table[i, 0] == table[i, 1] & table[i, 0] == table[i, 2])
+        if (board[i, 0] == board[i, 1] && board[i, 0] == board[i, 2] && board[i, 0] != ' ')
         {
-          returnValue = table[i, 0];
+          return board[i, 0];
         }
       }
-      if (table[0, 0] == table[1, 1] & table[0, 0] == table[2, 2])
+
+      // Diagonal checks
+      if (board[0, 0] == board[1, 1] && board[0, 0] == board[2, 2] && board[0, 0] != ' ')
       {
-        returnValue = table[0, 0];
+        return board[0, 0];
       }
-      else if (table[0, 2] == table[1, 1] & table[0, 2] == table[2, 0])
+      if (board[0, 2] == board[1, 1] && board[0, 2] == board[2, 0] && board[0, 2] != ' ')
       {
-        returnValue = table[0, 2];
+        return board[0, 2];
       }
-      return returnValue;
+
+      foreach (char cell in board)
+      {
+        if (cell == ' ')
+        {
+          return ' ';
+        }
+      }
+
+      return 'd';
     }
 
-    char crrPlayer = 'x';
-    char check = ' ';
-
-    Console.WriteLine(@"Hi! Good to see you\n
-                        Do you want to play ? S or N");
-    string startGame = Console.ReadLine();
-
-    while (startGame == "S")
+    void displayTable(char[,] board)
     {
-      Console.WriteLine($@" 1   2   3\n");
+      Console.WriteLine($"   1   2   3");
       for (int i = 0; i < 3; i++)
       {
-        Console.WriteLine($@"${i + 1} ${table[i, 0]}   |${table[i, 1]}   |${table[i, 2]}  \n -------------");
+        Console.WriteLine($"{i + 1}  {board[i, 0]} | {board[i, 1]} | {board[i, 2]}");
+        if (i < 2)
+        {
+          Console.WriteLine("  ---|---|---");
+        }
       }
-      int line = Convert.ToInt32(Console.ReadLine());
-      int column = Convert.ToInt32(Console.ReadLine());
+    }
 
-      if (table[line, column] == ' ')
+    char crrPlayer = 'X';
+    char result = ' ';
+
+    Console.WriteLine("Hi! Good to see you!\nDo you want to play? Y or N");
+    var startGame = Console.ReadLine();
+
+    if (startGame == "Y" || startGame == "y")
+    {
+      while (result == ' ')
       {
-        table[line, column] = crrPlayer;
-        check = checkWinner();
+        Console.Clear();
+        Console.WriteLine("--------------------------------------------------\n");
+        Console.WriteLine($"{crrPlayer}'s turn\n");
+        displayTable(table);
+
+        int line, column;
+        bool validInput;
+
+        do
+        {
+          Console.Write("Line number (1-3) => ");
+          validInput = int.TryParse(Console.ReadLine(), out line) && line >= 1 && line <= 3;
+          if (!validInput)
+          {
+            Console.WriteLine("Invalid input! Please enter a number between 1 and 3.");
+          }
+        } while (!validInput);
+
+        do
+        {
+          Console.Write("Column number (1-3) => ");
+          validInput = int.TryParse(Console.ReadLine(), out column) && column >= 1 && column <= 3;
+          if (!validInput)
+          {
+            Console.WriteLine("Invalid input! Please enter a number between 1 and 3.");
+          }
+        } while (!validInput);
+
+        if (table[line - 1, column - 1] != ' ')
+        {
+          Console.WriteLine("Invalid position! Please choose another spot.");
+          continue;
+        }
+
+        table[line - 1, column - 1] = crrPlayer;
+        result = checkWinner(table);
+
+        crrPlayer = crrPlayer == 'X' ? 'O' : 'X';
       }
-      if (check != ' ')
+
+      Console.Clear();
+      Console.WriteLine("--------------------------------------------------\n");
+      displayTable(table);
+      if (result == 'd')
       {
-        //
+        Console.WriteLine("It's a draw!");
       }
-      Console.WriteLine("Invalid position!");
+      else
+      {
+        Console.WriteLine($"{result} wins!");
+      }
     }
   }
 }
